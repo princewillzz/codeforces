c0d6039fd9cdd3688d7a29c19cf756d7e46b9e61
princewillzz <princewillz2013@gmail.com>
2020-04-20 15:26:07 +0530

    630A

---
+++ AgainTwentyFive630A.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AgainTwentyFive630A {
+    public static void main(String argv[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(25);
+
+    }
+    
+
+}
