24da31dc2f970bec3bb7c0458cca91fc5a84f7f9
princewillzz <princewillz2013@gmail.com>
2020-04-24 00:01:24 +0530

    contest636

--- codeforces636&635/constantpalin.java
+++ codeforces636&635/constantpalin.java
@@ -1,45 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class constantpalin {
-    public static void main(String argv[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(final String argv[]) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        while(t-- > 0) {
-            String s[] = br.readLine().split(" ");
-            String inp[] = br.readLine().split(" ");
-            int k = Integer.parseInt(s[1]);
-            int n =Integer.parseInt(s[0]);
-            int ar[] = new int[n];
-            for(int i = 0; i < n; i++) {
+        StringBuilder str = new StringBuilder();
+        while (t-- > 0) {
+            final String s[] = br.readLine().split(" ");
+            final String inp[] = br.readLine().split(" ");
+            final int k = Integer.parseInt(s[1]);
+            final int n = Integer.parseInt(s[0]);
+            final int ar[] = new int[n];
+            int max_pair = 0, b = 0;
+            for (int i = 0; i < n; i++) {
                 ar[i] = Integer.parseInt(inp[i]);
+                        
+            if (i < n / 2) {
+                final int curr = ar[i] + Integer.parseInt(inp[n - i - 1]);
+                max_pair = Math.min(curr, max_pair);
+                b = Math.max(curr, b);
+            } }
+            int ac_ans = Integer.MAX_VALUE;
+            while (max_pair <= b) {
+                max_pair++;
+                int ans = 0;
+                for (int i = 0, j = n - 1; i < j; i++, j--) {
+                    final int curr_pair = ar[i] + ar[j];
+                    if (curr_pair == max_pair)
+                        continue;
+                    if (curr_pair < max_pair) {
+                        final int diff = max_pair - curr_pair;
+                        if (ar[i] + diff <= k || ar[j] + diff <= k)
+                            ans++;
+                        else
+                            ans += 2;
+                    } else {
+                        final int diff = curr_pair - max_pair;
+                        if(ar[i]-diff >= 1 || ar[j]-diff >= 1) ans++;
+                        else ans+=2;
+                    };
+                }
+                ac_ans = Math.min(ac_ans, ans);
+                
             }
-            int a[] = new int[(2*k)+100], cnt[] = new int[(2*k)+100];
-            Arrays.fill(a, 0);Arrays.fill(cnt, 0);
-            for(int i = 0; i < n/2; i++) {
-                a[ar[i]+ar[n-i-1]]++;
-                int mn = Math.min(ar[i], ar[n-i-1]);
-                int mx = Math.max(ar[i], ar[n-i-1]);
-                mn++;mx+=k+1;
-                cnt[mn]++;
-                cnt[mx]--;
-            }
-            int ans = n;
-            for(int i = 1; i <= 2*k; i++) {
-                cnt[i] += cnt[i-1];
-                int cur = n - a[i] - cnt[i];
-                ans = Math.min(ans, cur);
-            }
-
-            
+        str.append(ac_ans + "\n");
         
-            System.out.println(ans);
         }
-
+        System.out.println(str);
+        
     }
-    
-    
-
 }
